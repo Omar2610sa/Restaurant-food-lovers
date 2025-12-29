201edbb10a9b4d2ddebfce7d2e59e05a65c741d4
Omar Salam <omarmooataz26@gmail.com>
2025-12-29 13:07:18 +0200

    Update: Add Menu list for menu section

---
+++ main.js
@@ -0,0 +1,18 @@
+const filterButton = document.querySelectorAll(".filter-btn")
+
+const menuItems = document.querySelectorAll(".item")
+
+
+filterButton.forEach((button) => {
+    button.addEventListener("click", ()=>{
+        const category = button.getAttribute("data-category")
+        
+        menuItems.forEach((item) => {
+            if (category === "all" || category === item.getAttribute("data-category")){
+                item.style.display = "flex"
+            }else {
+                item.style.display = "none"
+            }
+        })
+    })
+})
